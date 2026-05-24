299addbc8831db74e4d1451f7daf3e76c45e7f92
Chun-Chieh Chin <chun-chiehchin@Chun-ChiehdeMacBook-Pro.local>
2026-05-24 15:31:49 +0800

    chore: switch to vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   // 核心：必須告訴 Astro 這不是純靜態網站，我們要動態執行後端 API
   output: 'server', 
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: vercel(),
 });
